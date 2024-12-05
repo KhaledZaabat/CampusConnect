63abb7c1c0f426c7e3473ee7f3de88d2290d9a2b
Elhadi <mohamed.elhadi.bachir@ensia.edu.dz>
2024-12-05 11:19:12 +0100

    php files

--- Canteen.php
+++ Canteen.php
@@ -16,59 +16,9 @@
     <script src="assets/js/navbar.js"></script>
 </head>
 
-<header>
-    <nav class="navbar navbar-expand-lg fixed-top bg-body clean-navbar">
-        <div class="container">
-            <a class="navbar-brand-logo" href="#">
-                <img class="logo_img" src="assets/img/logo.png" alt="Brand Logo"> 
-            </a>
-            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1">
-                <span class="visually-hidden">Toggle navigation</span>
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="news.html">News</a></li>
-                    <li class="nav-item"><a class="nav-link" href="Canteen.html">Canteen Schedule</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
-                        <ul class="dropdown-menu" aria-labelledby="servicesDropdown">
-                            <li class="dropdown-header">Maintenance Services</li>
-                            <li><a class="dropdown-item" href="ReportIssues.html">Report Issues</a></li>
-                            <li><a class="dropdown-item" href="lostfound.html">Lost&Found items</a></li>
-                            <li class="dropdown-header">Housing Services</li>
-                            <li><a class="dropdown-item" href="BookRoom.html">Book Rooms</a></li>
-                            <li><a class="dropdown-item" href="changeRoom.html">Change Rooms</a></li>
-                        </ul>
-                    </li>
-                </ul>
 
-                <!-- profile section for pc -->
-                <div class="dropdown d-none d-lg-block me-3">
-                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="StudentProfile.html">Profile</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
-                </div>
-                
-
-                <!-- Profile and Sign-out links for smaller screens(phone) -->
-                <ul class="navbar-nav d-lg-none">
-                    <li><hr class="dropdown-divider my-1"></li>
-                    <li class="nav-item"><a class="nav-link" href="StudentProfile.html">Profile</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Sign out</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
 <body>
-
+    <?php include 'headerStud.php' ?>
     <h1>Canteen Schedule</h1>
 
     <table class="canteenTable">
--- roomRequests.php
+++ roomRequests.php
@@ -19,61 +19,11 @@
 
 </head>
 
-<header>
-    <nav class="navbar navbar-expand-lg fixed-top bg-body clean-navbar">
-        <div class="container">
-            <a class="navbar-brand-logo" href="#">
-                <img class="logo_img" src="assets/img/logo.png" alt="Brand Logo"> 
-            </a>
-            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1">
-                <span class="visually-hidden">Toggle navigation</span>
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item active"><a class="nav-link" href="AdminHome.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="AdminNews.html">News</a></li>
-                    <li class="nav-item"><a class="nav-link" href="CanteenManagement.html">Canteen Schedule</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
-                        <ul class="dropdown-menu" aria-labelledby="servicesDropdown">
-                            <li class="dropdown-header">Maintenance Services</li>
-                            <li><a class="dropdown-item" href="ReceivingIssues.html">Manage Issues</a></li>
-                            <li><a class="dropdown-item" href="lostfound.html">Lost&Found items</a></li>
-                            <li class="dropdown-header">Housing Services</li>
-                            <li><a class="dropdown-item" href="#">Book Rooms</a></li>
-                            <li><a class="dropdown-item" href="roomRequests.html">Change Rooms</a></li>
-                            <li class="dropdown-header">Users</li>
-                            <li><a class="dropdown-item" href="crudstud.html"> Manage Students</a></li>
-                            <li><a class="dropdown-item" href="crudadmin.html"> Manage Employees</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <!-- profile section for pc -->
-                <div class="dropdown d-none d-lg-block me-3">
-                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="StudentProfile.html">Profile</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
-                </div>
-                
 
-                <!-- Profile and Sign-out links for smaller screens(phone) -->
-                <ul class="navbar-nav d-lg-none">
-                    <li><hr class="dropdown-divider my-1"></li>
-                    <li class="nav-item"><a class="nav-link" href="UserProfile.html">Profile</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Sign out</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
     
 <body>
+<?php include 'headerAdmin.php' ?>
+
     <!-- Room Change Requests -->
     <div class="table-container">
         <div class="dropdown mb-3 text-end">
@@ -107,47 +57,11 @@
         <div id="pagination" class="d-flex justify-content-center mt-3">
         </div>
     </div>
+    <?php include 'footer.php' ?>
+
 </body>
 
-<footer class="page-footer custom-bg-black">
-    <div class="test">
-        <div class="wrapper">
-            <div class="button">
-                <div class="icon"><i class="fab fa-facebook-f"></i></div>
-                <span>Facebook</span>
-            </div>
 
-            <div class="button">
-                <div class="icon"><i class="fab fa-twitter"></i></div>
-                <span>Twitter</span>
-            </div>
-
-            <div class="button">
-                <div class="icon"><i class="fab fa-youtube"></i></div>
-                <span>Youtube</span>
-            </div>
-
-            <div class="button">
-                <div class="icon"><i class="fab fa-github"></i></div>
-                <span>Github</span>
-            </div>
-            
-            <div class="button">
-                <div class="icon"><i class="fab fa-instagram"></i></div>
-                <span>Instagram</span>
-            </div>
-        </div>
-
-        <div class="footer-image">
-            <img src="assets/img/logo.png" alt="Footer Image" />
-        </div>
-
-    </div>
-    
-    <div class="text-center custom-bg-black p-1">
-        <p>©ENSIA. All Rights Reserved.</p>
-    </div>
-</footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/roomsrequests.js"></script>
     <script src="assets/js/helperFunctions.js"></script>
